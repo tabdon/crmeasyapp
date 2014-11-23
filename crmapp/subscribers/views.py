7ed12facca2f94eb8bba721e9b11882ea24726fe
Tobias Abdon <tabdon@gmail.com>
2014-11-23 14:12:24 +0800

    Create the Subscriber Form - Part II > Update the View

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
 from .forms import SubscriberForm
+from .models import Subscriber
 
 def subscriber_new(request, template='subscribers/subscriber_new.html'):
     if request.method == 'POST':
@@ -12,11 +13,21 @@ def subscriber_new(request, template='subscribers/subscriber_new.html'):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             # Create the User record
-            user = User(username=username, email=email)
+            user = User(username=username, email=email,
+                        first_name=first_name, last_name=last_name)
             user.set_password(password)
             user.save()
             # Create Subscriber Record
+            address_one = form.cleaned_data['address_one']
+            address_two = form.cleaned_data['address_two']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            sub = Subscriber(address_one=address_one, address_two=address_two,
+                             city=city, state=state, user_rec=user)
+            sub.save()
             # Process payment (via Stripe)
             # Auto login the user
             return HttpResponseRedirect('/success/')
