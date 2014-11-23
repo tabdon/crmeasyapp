6be102bea1458b957fd735742ffc11ed9d81bdb3
Tobias Abdon <tabdon@gmail.com>
2014-11-23 15:24:43 +0800

    Create the Contacts App - Part II > Edit Contact - Create URL

--- crmapp/contacts/urls.py
+++ crmapp/contacts/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 contact_urls = patterns('',
 
     url(r'^$', 'crmapp.contacts.views.contact_detail', name="contact_detail"),
+    url(r'^edit/$',
+        'crmapp.contacts.views.contact_cru', name='contact_update'
+    ),
 
 )
