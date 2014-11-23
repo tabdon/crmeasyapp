bca81515ef78cf3b52ad853b7d7796918ad1003c
Tobias Abdon <tabdon@gmail.com>
2014-11-23 14:34:38 +0800

    Create the Subscriber Form - Part III > Update the View

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.forms.forms import NON_FIELD_ERRORS
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from .forms import SubscriberForm
 from .models import Subscriber
+
+import stripe
+
 
 def subscriber_new(request, template='subscribers/subscriber_new.html'):
     if request.method == 'POST':
@@ -29,9 +36,30 @@ def subscriber_new(request, template='subscribers/subscriber_new.html'):
                              city=city, state=state, user_rec=user)
             sub.save()
             # Process payment (via Stripe)
+            fee = settings.SUBSCRIPTION_PRICE
+            try:
+                stripe_customer = sub.charge(request, email, fee)
+            except stripe.StripeError as e:
+                form._errors[NON_FIELD_ERRORS] = form.error_class([e.args[0]])
+                return render(request, template,
+                    {'form':form,
+                     'STRIPE_PUBLISHABLE_KEY':settings.STRIPE_PUBLISHABLE_KEY}
+                )
             # Auto login the user
-            return HttpResponseRedirect('/success/')
+            a_u = authenticate(username=username, password=password)
+            if a_u is not None:
+                if a_u.is_active:
+                    login(request, a_u)
+                    return HttpResponseRedirect(reverse('account_list'))
+                else:
+                    return HttpResponseRedirect(
+                        reverse('django.contrib.auth.views.login')
+                    )
+            else:
+                return HttpResponseRedirect(reverse('sub_new'))
     else:
         form = SubscriberForm()
 
-    return render(request, template, {'form':form})
+    return render(request, template,
+        {'form':form,
+         'STRIPE_PUBLISHABLE_KEY':settings.STRIPE_PUBLISHABLE_KEY})
