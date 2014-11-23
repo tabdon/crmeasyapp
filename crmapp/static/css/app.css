ff90c7c8d52b5367e89c7b299f23ff3e7648d3c6
Tobias Abdon <tabdon@gmail.com>
2014-11-23 12:44:59 +0800

    Setup CRM Easy Project > Add Static Files

---
+++ crmapp/static/css/app.css
@@ -0,0 +1,620 @@
+/* Helper Classes */
+
+
+.side-p {
+    padding: 0 24px;
+}
+
+
+.p-none {
+    padding: 0 !important;
+}
+
+
+.m-none {
+    margin: 0 !important;
+}
+
+
+.p-l-none {
+    padding-left: 0 !important;
+}
+
+
+.p-r-none {
+    padding-right: 0 !important;
+}
+
+
+.p-t-none {
+    padding-top: 0 !important;
+}
+
+
+.p-b-none {
+    padding-bottom: 0 !important;
+}
+
+
+.m-l-none {
+    margin-left: 0 !important;
+}
+
+
+.m-r-none {
+    margin-right: 0 !important;
+}
+
+
+.m-t-none {
+    margin-top: 0 !important;
+}
+
+
+.m-b-none {
+    margin-bottom: 0 !important;
+}
+
+
+.errorlist {
+    padding: 0;
+    list-style-type: none;
+    color: darkred;
+}
+
+
+.strip-list {
+    list-style-type: none;
+}
+
+
+/* Base.html Styles */
+
+
+html, body {
+    height: 100%;
+    width: 100%;
+}
+
+
+body {
+    background-color: #f7fbfd;
+    font-family: 'MYRIADPRO-REGULAR',Helvetica,Arial,sans-serif !important;
+}
+
+h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
+    font-family: 'MYRIADPRO-REGULAR',Helvetica,Arial,sans-serif !important;
+}
+
+
+#site-wrapper {
+    min-height: 100%;
+    width: 100%;
+    /*background-color: #f5fafc;*/
+    display: inline-block;
+}
+
+
+#content-container {
+    min-height: 900px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-color: white;
+    border: 1px solid #cccccc;
+}
+
+
+.footer {
+    background: #353F45;
+    color: white;
+    text-align: center;
+    padding: 10px;
+    width: 100%;
+}
+
+
+/* Account List & Detail Pages */
+
+
+.ad-mh {
+    font-weight: normal;
+}
+
+
+#new-contact {
+    padding: 6px 24px;
+}
+
+
+.ci-td-p-r {
+    padding: 12px 24px 0 12px !important;
+    width: 10px;
+}
+
+
+.ci-td-p-l {
+    padding-left: 24px !important;
+}
+
+
+.ad-control {
+    float: right;
+    display: inline-block;
+    padding: 9px 0;
+    font-size: 12px;
+}
+
+
+.gi-body {
+    clear: both;
+}
+
+
+.nm {
+    margin: 0;
+}
+
+
+.gi-sh {
+    margin-bottom: 5px;
+}
+
+
+#gi-form textarea#id_desc {
+    height: 138px;
+}
+
+
+.gi-form-addr {
+    height: 25px;
+    padding-left: 3px;
+    margin-bottom: 3px;
+}
+
+
+#gi-form .errorlist {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    color: red;
+}
+
+
+#gi-form-submit {
+    float: right;
+}
+
+
+.ad-container {
+    margin-bottom: 72px;
+}
+
+
+.ad-headers {
+    background-color: #808080;
+    padding: 6px 24px;
+    color: #f1f1f1;
+}
+
+
+.gi-header {
+    border-bottom: 1px solid #bfbfbf;
+}
+
+
+#gi-container {
+    padding: 0 24px;
+}
+
+
+#gi-form .gi-mh {
+    width: 100%;
+}
+
+
+#cd-body {
+    width: 100%;
+    margin: 0 auto;
+}
+
+
+#cd-table {
+    margin-bottom: 0;
+}
+
+
+#cd-table td {
+    border-top: none;
+    border-bottom: 1px solid #dddddd;
+}
+
+
+.co-item-date {
+    font-size: 12px;
+    color: #ccc;
+    font-style: italic;
+}
+
+
+.co-item {
+    border-bottom: 1px solid #ccc;
+    padding: 12px 24px;
+}
+
+
+.co-item-h {
+    margin-bottom: 3px;
+}
+
+
+#comm-form {
+    margin-top: 20px;
+}
+
+
+#comm-form td {
+    border-top: none !important;
+}
+
+
+.comm-submit {
+    float: right;
+}
+
+
+#comm-form-internals {
+    display: none;
+}
+
+
+#co-form-wrapper {
+    padding: 0 24px;
+}
+
+
+.comm-submit-td {
+    padding-right: 0 !important;
+}
+
+
+.pen {
+    background-image: url('../images/pen.png');
+    width: 10px;
+    height: 10px;
+    display: block;
+}
+
+
+.cancel {
+    background-image: url('../images/cancel.png');
+    width: 10px;
+    height: 10px;
+    display: block;
+}
+
+
+.comm-control {
+    float: right;
+    margin-left: 10px;
+}
+
+
+#contact-form {
+    padding: 0 24px;
+}
+
+
+#contact-form td {
+    border: none;
+}
+
+
+#contact-form table {
+    margin-bottom: 0;
+}
+
+
+#contact-submit {
+    float: right;
+    margin-right: 8px;
+}
+
+
+#al-container {
+    position: relative;
+    min-height: 660px;
+}
+
+
+#al-control {
+    margin-top: 20px;
+}
+
+
+#al-search {
+    width: 88.5%;
+    display: inline-block;
+}
+
+
+#al-group {
+    padding: 0;
+    margin-top: 24px;
+}
+
+
+.al-item {
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 18px;
+    padding-bottom: 5px;
+}
+
+
+.al-name {
+    display: inline;
+}
+
+
+.al-created {
+    float: right;
+}
+
+
+#al-paginate ul {
+    padding: 0;
+}
+
+
+.con-cntrl {
+    padding: 12px 0 0 6px !important;
+    width: 10px;
+}
+
+.con-cntrl a {
+    float: right;
+}
+
+
+/* Signup Page */
+
+
+#payment-form table td {
+    border: none;
+    padding: 5px 15px 0 0;
+}
+
+
+#payment-form table th {
+    border: none;
+    padding: 6px 0 0 0;
+}
+
+
+.sn-sh {
+    margin-top: 30px;
+    padding-bottom: 6px;
+}
+
+
+#payment-submit {
+    float: right;
+    margin-bottom: 24px;
+    margin-right: 15px;
+}
+
+
+/* Marketing Page Styles */
+
+
+#mkt-container {
+    min-height: 100%;
+}
+
+
+.mkt-nav {
+    height: 66px;
+    background-color: #f7fbfd;
+    border: none;
+}
+
+
+.mkt-nav .navbar-brand {
+    color: #d35301;
+    font-size: 24px;
+}
+
+.mkt-nav .navbar-brand:hover {
+    color: #F8501C;
+}
+
+
+.mkt-nav .navbar-collapse ul li a {
+    color: #425c71;
+}
+
+
+.mkt-s1 {
+    background-color: #425c71;
+}
+
+
+.mkt-s1-body {
+    color: #f3f8fb;
+}
+
+
+.sign-up {
+    margin: 24px 15px;
+    font-size: 18px;
+}
+
+
+.mkt-vp {
+    text-align: center;
+    margin-top: 60px;
+}
+
+
+.mkt-s2-h {
+    margin-top: 60px;
+    text-align: center;
+}
+
+
+.mkt-s2-body {
+    display: inline-block;
+}
+
+
+.sgn-btm {
+    display: block;
+    text-align: center;
+    font-size: 18px;
+    text-decoration: underline;
+    margin-top: 60px;
+    margin-bottom: 30px;
+}
+
+
+/* Login Page */
+
+
+.lgn-container {
+    margin: 30px auto;
+    float: none;
+}
+
+
+
+
+.p-body {
+    background-color: #DFE5E8;
+    /*overflow: hidden;*/
+}
+
+.p-sidebar {
+    background-color: #252D2F;
+    color: #FBFEFF;
+    min-height: 1075px;
+    /*margin-bottom: -9999px;*/
+    /*padding-bottom: 9999px;*/
+}
+
+/*#site-wrapper {*/
+    /*margin-bottom: -9999px;*/
+    /*padding-bottom: 9999px;*/
+/*}*/
+
+.p-sidebar-title {
+    background-color: #1A2021;
+    padding: 20px 0 10px;
+}
+
+.p-sidebar-title h2 {
+    padding: 15px;
+    font-size: 20px;
+    margin: 0;
+}
+
+.p-sidebar-body {
+    padding-top: 15px;
+}
+
+.p-sidebar-body a {
+    color: #FBFEFF;
+}
+
+.p-sidebar-body h3 {
+    font-size: 16px;
+    font-family: 'MYRIADPRO-REGULAR',Helvetica,Arial,sans-serif;
+}
+
+.p-sidebar-body p {
+    font-size: 14px;
+    font-family: 'MYRIADPRO-REGULAR',Helvetica,Arial,sans-serif;
+}
+
+.lgn-container {
+    margin: 30px auto;
+    float: none;
+}
+
+#show-feature-list {
+    margin-left: 15px;
+}
+
+.p-fmap-container {
+    background-color: #F5F6F9;
+    margin-top: 54px;
+}
+
+.p-fmap-box {
+    border: 1px solid #E1E6E7;
+    box-shadow: 0px 1px 4px #C6CBCF;
+    background-color: white;
+    margin-bottom: 45px;
+    height: 66px;
+    color: #1A68E0;
+}
+
+.p-fmap-box h5 {
+}
+
+.p-fmap-box-href:hover {
+    text-decoration: none;
+}
+
+.p-fmap-box p {
+    display: inline;
+}
+
+.p-fmap-box p span {
+    margin-left: 20px;
+}
+
+.p-fmap-box span.glyphicon {
+    float: right;
+    font-size: 36px;
+    margin-top: -20px;
+}
+
+.p-fmap-box-active {
+    background-color: #306DE2 !important;
+    color: white !important;
+}
+
+.p-small-card {
+    padding: 0 15px;
+    margin-top: 54px;
+    border: 1px solid #E1E6E7;
+    box-shadow: 0px 1px 4px #C6CBCF;
+    background-color: white;
+    height: 66px;
+    color: #1A68E0;
+}
+
+.p-lesson-body {
+    margin-top: 30px;
+}
+
+.p-start-lessons {
+    display: block;
+    padding: 10px 15px;
+    margin: 0 -15px;
+    background-color: #306DE2;
+}
+
+.p-start-lessons span {
+    margin-right: 10px;
+}
+
+.p-see-card-map {
+    margin-top: 15px;
+    display: block;
+}
+
+.p-lesson-list {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+}
