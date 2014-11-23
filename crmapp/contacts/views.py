1d7528f2b9afd030b62f0e43c9b47446224e5515
Tobias Abdon <tabdon@gmail.com>
2014-11-23 15:15:54 +0800

    Create the Contacts App - Part I > View Contact - Create View

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Contact
+
+
+@login_required()
+def contact_detail(request, uuid):
+
+    contact = Contact.objects.get(uuid=uuid)
+
+    return render(request, 
+                'contacts/contact_detail.html', 
+                {'contact': contact}
+    )
