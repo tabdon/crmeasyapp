7064916ddd2913856b9493670ca2d525fd412b06
Tobias Abdon <tabdon@gmail.com>
2014-11-23 14:40:49 +0800

    Create the Login Page > Create the Login & Logout URLs

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -19,7 +19,12 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 
     # Login/Logout URLs
-
+    (r'^login/$',
+        'django.contrib.auth.views.login', {'template_name': 'login.html'}
+    ),
+    (r'^logout/$',
+        'django.contrib.auth.views.logout', {'next_page': '/login/'}
+    ),
 
     # Account related URLs
 
