fbd7da82e8231c025eaaf9dd60f94d104583c02c
Tobias Abdon <tabdon@gmail.com>
2014-11-23 15:06:26 +0800

    Create the Account Detail Page - Part II > Edit Account - Create URL Conf

--- crmapp/accounts/urls.py
+++ crmapp/accounts/urls.py
@@ -5,4 +5,7 @@ account_urls = patterns('',
     url(r'^$',
         'crmapp.accounts.views.account_detail', name='account_detail'
     ),
+    url(r'^edit/$',
+        'crmapp.accounts.views.account_cru', name='account_update'
+    ),
 )
