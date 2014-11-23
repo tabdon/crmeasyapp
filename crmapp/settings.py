c42d0ea8f91e81b29f91ecc5ed94292af60d8c9f
Tobias Abdon <tabdon@gmail.com>
2014-11-23 12:41:14 +0800

    Setup CRM Easy Project > Serving Static Files

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -102,3 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- crmapp/wsgi.py
+++ crmapp/wsgi.py
@@ -4,11 +4,14 @@ WSGI config for crmapp project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crmapp.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
