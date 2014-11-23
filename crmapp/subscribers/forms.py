97f021e98fe17fd4a7db6306c2d89a0793947295
Tobias Abdon <tabdon@gmail.com>
2014-11-23 14:11:11 +0800

    Create the Subscriber Form - Part II > Update the Form

--- crmapp/subscribers/forms.py
+++ crmapp/subscribers/forms.py
@@ -1,7 +1,27 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-class SubscriberForm(UserCreationForm):
+from .models import Subscriber
+
+
+class AddressMixin(forms.ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ('address_one', 'address_two', 'city', 'state',)
+        widgets = {
+            'address_one': forms.TextInput(attrs={'class':'form-control'}),
+            'address_two': forms.TextInput(attrs={'class':'form-control'}),
+            'city': forms.TextInput(attrs={'class':'form-control'}),
+            'state': forms.TextInput(attrs={'class':'form-control'}),
+        }
+
+class SubscriberForm(AddressMixin, UserCreationForm):
+    first_name = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    last_name = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-control'})
+    )
     email = forms.EmailField(
         required=True, widget=forms.TextInput(attrs={'class':'form-control'})
     )
