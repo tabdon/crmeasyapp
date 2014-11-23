9aaa6a6d194bee5aa0d13fd2e58f2392145f72bc
Tobias Abdon <tabdon@gmail.com>
2014-11-23 14:50:48 +0800

    Create the Account List > List Accounts - Create View

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+from .models import Account
+
+class AccountList(ListView):
+    model = Account
+    template_name = 'accounts/account_list.html'
+    context_object_name = 'accounts'
+
+    def get_queryset(self):
+        account_list = Account.objects.filter(owner=self.request.user)
+        return account_list
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(AccountList, self).dispatch(*args, **kwargs)
