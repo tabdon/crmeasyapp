6fc0c3884c38448956273d99a57e0c758ecbc658
Tobias Abdon <tabdon@gmail.com>
2014-11-23 13:20:19 +0800

    Create the Home Page > Create the Home Page View

--- crmapp/marketing/views.py
+++ crmapp/marketing/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class HomePage(TemplateView):
+    """
+    Because our needs are so simple, all we have to do is
+    assign one value; template_name. The home.html file will be created
+    in the next lesson.
+    """
+    template_name = 'marketing/home.html'
