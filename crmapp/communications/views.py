3bc9a062f548eef3e683833e75daf715df5c5403
Tobias Abdon <tabdon@gmail.com>
2014-11-23 18:24:34 +0800

    Create the Communications App - Part I > View Comm - Create View

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
-# Create your views here.
+from .models import Communication
+
+@login_required()
+def comm_detail(request, uuid):
+
+    comm = Communication.objects.get(uuid=uuid)
+    if comm.owner != request.user:
+            return HttpResponseForbidden()
+
+    return render(request, 'communications/comm_detail.html', {'comm':comm})
