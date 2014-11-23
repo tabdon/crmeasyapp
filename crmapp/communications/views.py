e811874fea6ecd179bc0e029692e268381c42adc
Tobias Abdon <tabdon@gmail.com>
2014-11-23 18:38:31 +0800

    Create the Communications App - Part II > Edit Comm - Modify View

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 
 from .models import Communication
 from .forms import CommunicationForm
+from crmapp.accounts.models import Account
 
 @login_required()
 def comm_detail(request, uuid):
@@ -17,10 +19,17 @@ def comm_detail(request, uuid):
     return render(request, 'communications/comm_detail.html', {'comm':comm})
 
 @login_required()
-def comm_cru(request):
+def comm_cru(request, uuid=None, account=None):
+
+    if uuid:
+        comm = get_object_or_404(Communication, uuid=uuid)
+        if comm.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        comm = Communication(owner=request.user)
 
     if request.POST:
-        form = CommunicationForm(request.POST)
+        form = CommunicationForm(request.POST, instance=comm)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -36,11 +45,20 @@ def comm_cru(request):
                 args=(account.uuid,)
             )
             return HttpResponseRedirect(reverse_url)
+        else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
     else:
-        form = CommunicationForm()
+        form = CommunicationForm(instance=comm)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'comm':comm,
+        'account': account
     }
 
     template = 'communications/comm_cru.html'
