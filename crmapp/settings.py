98ebcdbd81a5b463f12deb078cba1b963034afe6
Tobias Abdon <tabdon@gmail.com>
2014-11-23 12:33:15 +0800

    Setup CRM Easy Project > Configure the Database

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'crmapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmeasyDB',
+        'USER': 'postgres',
+        'PASSWORD': 'warning this is where you need to put your password',
+        'HOST': '/tmp',
+        'PORT': '5432',
     }
 }
 
