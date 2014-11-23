a541fc3fec34c2ca91e32d25bbb2aed0a2db4db3
Tobias Abdon <tabdon@gmail.com>
2014-11-23 18:21:25 +0800

    Create the Communications App - Part I > Comm App Setup - Create Model

--- crmapp/communications/admin.py
+++ crmapp/communications/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Communication
 
-# Register your models here.
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'uuid')
+
+admin.site.register(Communication, CommunicationAdmin)
